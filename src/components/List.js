9738516cc6aefe31910a7a8a8a3901f5d2980ae2
JGardener <jamesgardener92@gmail.com>
2019-10-11 12:36:52 +0100

    Changed background colour of toDo-item to green on hover. Removed delete button and added this functionality to onClick of toDo-item div. Also attempted to convert List function to a class. Still a work-in-progress as I learn more ReactJS.

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,53 @@
 import React from "react";
 import ListItem from "../components/ListItem";
 
+// class List extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       title: props.title,
+//       subTitle: props.subTitle,
+//       toDos: props.toDos,
+//       createNewToDo: props.createNewToDo,
+//       removeToDo: props.removeToDo
+//     };
+//   }
+
+//   appendNewToDo = e => {
+//     if (e.key === "Enter") {
+//       createNewToDo(e.target.value);
+//       e.target.value = "";
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className="list-container">
+//         <div className="title-container">
+//           <h1>{this.state.title}</h1>
+//           <h6>{this.state.subTitle}</h6>
+//         </div>
+//         <div className="create-new-toDo">
+//           <input
+//             type="text"
+//             onKeyPress={this.appendNewToDo}
+//             placeholder='Press "Enter" to add your new to-do.'
+//           />
+//         </div>
+//         <ul className="toDo-list">
+//           {this.state.toDos.map(newToDo => (
+//             <ListItem
+//               key={newToDo}
+//               newToDo={newToDo}
+//               createNewToDo={this.state.createNewToDo}
+//               removeToDo={this.state.removeToDo}
+//             />
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 const List = ({ title, subTitle, toDos, createNewToDo, removeToDo }) => {
   const appendNewToDo = e => {
     if (e.key === "Enter") {
