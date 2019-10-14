ab36e1e1114b3ac3889bb061a65a23193fbaa335
Craig Salajan <csalajan@nexient.com>
2019-10-14 11:10:34 -0400

    Adding list-in-list feature

--- src/components/List.js
+++ src/components/List.js
@@ -1,81 +1,82 @@
 import React from "react";
 import ListItem from "../components/ListItem";
+import CreateToDo from "./CreateToDo";
 
-// class List extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       title: props.title,
-//       subTitle: props.subTitle,
-//       toDos: props.toDos,
-//       createNewToDo: props.createNewToDo,
-//       removeToDo: props.removeToDo
-//     };
-//   }
+class List extends React.Component {
+    state = {
+        toDos: [],
+    };
 
-//   appendNewToDo = e => {
+    createNewToDo = newToDo => {
+        this.setState({
+            toDos: [
+                ...this.state.toDos,
+                newToDo
+            ]
+        })
+    };
+
+    removeToDo = newToDo => {
+        const toDos = [...this.state.toDos];
+        const index = toDos.indexOf(newToDo);
+
+        if (index > -1) {
+            toDos.splice(index, 1);
+            this.setState({
+                toDos: toDos
+            });
+        }
+    };
+
+    render() {
+        return (
+          <div className="list-container">
+            <div className="title-container">
+              <h1>{this.props.title}</h1>
+              <h6>{this.props.subTitle}</h6>
+            </div>
+            <CreateToDo createNewToDo={this.createNewToDo} />
+            <ul className="toDo-list">
+              {this.state.toDos.map(toDo => (
+                <ListItem
+                  key={toDo}
+                  newToDo={toDo}
+                  removeToDo={this.removeToDo}
+                />
+              ))}
+            </ul>
+          </div>
+        );
+    }
+}
+// const List = ({ title, subTitle, toDos, createNewToDo, removeToDo }) => {
+//   const appendNewToDo = e => {
 //     if (e.key === "Enter") {
 //       createNewToDo(e.target.value);
 //       e.target.value = "";
 //     }
 //   };
-
-//   render() {
-//     return (
-//       <div className="list-container">
-//         <div className="title-container">
-//           <h1>{this.state.title}</h1>
-//           <h6>{this.state.subTitle}</h6>
-//         </div>
-//         <div className="create-new-toDo">
-//           <input
-//             type="text"
-//             onKeyPress={this.appendNewToDo}
-//             placeholder='Press "Enter" to add your new to-do.'
-//           />
-//         </div>
-//         <ul className="toDo-list">
-//           {this.state.toDos.map(newToDo => (
-//             <ListItem
-//               key={newToDo}
-//               newToDo={newToDo}
-//               createNewToDo={this.state.createNewToDo}
-//               removeToDo={this.state.removeToDo}
-//             />
-//           ))}
-//         </ul>
+//
+//   return (
+//     <div className="list-container">
+//       <div className="title-container">
+//         <h1>{title}</h1>
+//         <h6>{subTitle}</h6>
 //       </div>
-//     );
-//   }
-// }
-const List = ({ title, subTitle, toDos, createNewToDo, removeToDo }) => {
-  const appendNewToDo = e => {
-    if (e.key === "Enter") {
-      createNewToDo(e.target.value);
-      e.target.value = "";
-    }
-  };
-
-  return (
-    <div className="list-container">
-      <div className="title-container">
-        <h1>{title}</h1>
-        <h6>{subTitle}</h6>
-      </div>
-      <div className="create-new-toDo">
-        <input
-          type="text"
-          onKeyPress={appendNewToDo}
-          placeholder='Press "Enter" to add your new to-do.'
-        />
-      </div>
-      <ul className="toDo-list">
-        {toDos.map(newToDo => (
-          <ListItem key={newToDo} newToDo={newToDo} removeToDo={removeToDo} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+//       <div className="create-new-toDo">
+//         <input
+//           type="text"
+//           onKeyPress={appendNewToDo}
+//           placeholder='Press "Enter" to add your new to-do.'
+//         />
+//       </div>
+//       <ul className="toDo-list">
+//         {toDos.map(newToDo => (
+//           <ListItem key={newToDo} newToDo={newToDo} removeToDo={removeToDo} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default List;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,18 @@
 import React from "react";
+import List from "./List";
 
 const ListItem = ({ newToDo, removeToDo }) => {
   return (
-    <li className="toDo-item" onClick={() => removeToDo(newToDo)}>
-      <div className="toDo-label">{newToDo}</div>
+    <li className="toDo-item">
+        {newToDo.isList ?
+            <List
+                title={newToDo.text}
+            />
+
+        :
+            <div className="toDo-label">{newToDo.text}</div>
+        }
+        <button onClick={() => {removeToDo(newToDo)}}>Remove</button>
     </li>
   );
 };
