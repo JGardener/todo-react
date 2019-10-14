61f3e150e3d1e9fa445753b1e476523d0f055648
JGardener <jamesgardener92@gmail.com>
2019-10-14 15:07:24 +0100

    Converted List to a class component in order to handle Lists within Lists more efficiently. Learned by trial and error about props and the correct syntax for their usage.

--- src/components/List.js
+++ src/components/List.js
@@ -1,81 +1,72 @@
 import React from "react";
 import ListItem from "../components/ListItem";
 
-// class List extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       title: props.title,
-//       subTitle: props.subTitle,
-//       toDos: props.toDos,
-//       createNewToDo: props.createNewToDo,
-//       removeToDo: props.removeToDo
-//     };
-//   }
+class List extends React.Component {
+  appendNewToDo = e => {
+    if (e.key === "Enter") {
+      this.props.createNewToDo(e.target.value);
+      e.target.value = "";
+    }
+  };
 
-//   appendNewToDo = e => {
+  render() {
+    return (
+      <div className="list-container">
+        <div className="title-container">
+          <h1>{this.props.title}</h1>
+          <h6>{this.props.subTitle}</h6>
+        </div>
+        <div className="create-new-toDo">
+          <input
+            type="text"
+            onKeyPress={this.appendNewToDo}
+            placeholder='Press "Enter" to add your new to-do.'
+          />
+        </div>
+        <ul className="toDo-list">
+          {this.props.toDos.map(newToDo => (
+            <ListItem
+              key={newToDo}
+              newToDo={newToDo}
+              createNewToDo={this.props.createNewToDo}
+              removeToDo={this.props.removeToDo}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+// == THE OLD WAY YOU DID IT. WE LEARNED SOMETHING TODAY BOYS!
+// == Moved away from a functional component, to a class due to a requirement of handling lists within lists.
+// const List = ({ title, subTitle, toDos, createNewToDo, removeToDo }) => {
+//   const appendNewToDo = e => {
 //     if (e.key === "Enter") {
 //       createNewToDo(e.target.value);
 //       e.target.value = "";
 //     }
 //   };
 
-//   render() {
-//     return (
-//       <div className="list-container">
-//         <div className="title-container">
-//           <h1>{this.state.title}</h1>
-//           <h6>{this.state.subTitle}</h6>
-//         </div>
-//         <div className="create-new-toDo">
-//           <input
-//             type="text"
-//             onKeyPress={this.appendNewToDo}
-//             placeholder='Press "Enter" to add your new to-do.'
-//           />
-//         </div>
-//         <ul className="toDo-list">
-//           {this.state.toDos.map(newToDo => (
-//             <ListItem
-//               key={newToDo}
-//               newToDo={newToDo}
-//               createNewToDo={this.state.createNewToDo}
-//               removeToDo={this.state.removeToDo}
-//             />
-//           ))}
-//         </ul>
+//   return (
+//     <div className="list-container">
+//       <div className="title-container">
+//         <h1>{title}</h1>
+//         <h6>{subTitle}</h6>
 //       </div>
-//     );
-//   }
-// }
-const List = ({ title, subTitle, toDos, createNewToDo, removeToDo }) => {
-  const appendNewToDo = e => {
-    if (e.key === "Enter") {
-      createNewToDo(e.target.value);
-      e.target.value = "";
-    }
-  };
-
-  return (
-    <div className="list-container">
-      <div className="title-container">
-        <h1>{title}</h1>
-        <h6>{subTitle}</h6>
-      </div>
-      <div className="create-new-toDo">
-        <input
-          type="text"
-          onKeyPress={appendNewToDo}
-          placeholder='Press "Enter" to add your new to-do.'
-        />
-      </div>
-      <ul className="toDo-list">
-        {toDos.map(newToDo => (
-          <ListItem key={newToDo} newToDo={newToDo} removeToDo={removeToDo} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+//       <div className="create-new-toDo">
+//         <input
+//           type="text"
+//           onKeyPress={appendNewToDo}
+//           placeholder='Press "Enter" to add your new to-do.'
+//         />
+//       </div>
+//       <ul className="toDo-list">
+//         {toDos.map(newToDo => (
+//           <ListItem key={newToDo} newToDo={newToDo} removeToDo={removeToDo} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default List;
